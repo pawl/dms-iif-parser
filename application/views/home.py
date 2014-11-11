3020049b5b17a3d922fc92b4c08a0cc32770d758
Paul Brown <paul90brown@gmail.com>
2014-11-10 22:11:22 -0600

    do not save or show blank secret

--- application/views/home.py
+++ application/views/home.py
@@ -1,10 +1,16 @@
 from flask.ext.admin import AdminIndexView, expose
-from flask import request, session
+from flask import request, session, flash
 
 class HomeView(AdminIndexView):
 	@expose('/', methods=["GET","POST"])
 	def index(self):
 		secret = session.get('secret')
+		
 		if request.method == "POST":
-			session['secret'] = request.form.get('secret') 
+			secret = request.form.get('secret')
+			if secret:
+				session['secret'] = secret
+			else:
+				flash("Secret Key cannot be blank.")
+				
 		return self.render('home.html', secret=secret)
